d174816137bfbd88e498173f6d2b53d64ae13b72
Toai Vo <toaivo@Toais-Mac-mini.local>
2017-07-05 21:15:00 -0700

    OEL with Minsec

--- userdata-minsec.sh
+++ userdata-minsec.sh
@@ -22,31 +22,11 @@ sudo /bin/timedatectl set-timezone America/Los_Angeles
 
 # create user accounts
 
-sudo groupadd -g 3000 toaivo
-sudo groupadd -g 3001 chom
-sudo groupadd -g 3002 honglee
-sudo groupadd -g 3003 sanjeevk
-sudo groupadd -g 3004 dilipk
-sudo groupadd -g 3005 kmadurri
-sudo useradd -u 3000 -g 3000 -d /home/toaivo -s /bin/bash -c toaivo-user toaivo
-sudo useradd -u 3001 -g 3001 -d /home/chom -s /bin/bash -c calvin-user chom
-sudo useradd -u 3002 -g 3002 -d /home/honglee -s /bin/bash -c Stanley-user honglee
-sudo useradd -u 3003 -g 3003 -d /home/sanjeevk -s /bin/bash -c sanjeev-user sanjeevk
-sudo useradd -u 3004 -g 3004 -d /home/dilipk -s /bin/bash -c dilip-user dilipk
-sudo useradd -u 3005 -g 3005 -d /home/kmadduri -s /bin/bash -c kumar-user kmadduri
-sudo echo 'toaivo ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/90-cloud-init-users
-sudo echo 'chom ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/90-cloud-init-users
-sudo echo 'honglee ALL=(ALL) NOPASSWD:ALL' | sudo tee -a  /etc/sudoers.d/90-cloud-init-users
-sudo echo 'sanjeevk ALL=(ALL) NOPASSWD:ALL' | sudo tee -a  /etc/sudoers.d/90-cloud-init-users
-sudo echo 'dilipk ALL=(ALL) NOPASSWD:ALL' | sudo tee -a  /etc/sudoers.d/90-cloud-init-users
-sudo echo 'kmadduri ALL=(ALL) NOPASSWD:ALL' | sudo tee -a  /etc/sudoers.d/90-cloud-init-users
+sudo groupadd -g 3000 tonyvo
+sudo useradd -u 3000 -g 3000 -d /home/tonyvo -s /bin/bash -c tonyvo-user tonyvo
+sudo echo 'tonyvo ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/90-cloud-init-users
 sudo groupadd -g 44398 duo
-sudo usermod -G toaivo,duo toaivo
-sudo usermod -G honglee,duo honglee
-sudo usermod -G chom,duo chom
-sudo usermod -G sanjeevk,duo sanjeevk
-sudo usermod -G dilipk,duo dilipk
-sudo usermod -G kmadduri,duo kmadduri
+sudo usermod -G tonyvo,duo tonyvo
  
 #  setup duosecurity repository to install duo
  
@@ -70,7 +50,7 @@ sudo yum -y install ossec-hids ossec-hids-client
 
 #  get Stanford Specific Configuration file from s3
 sudo mkdir -p /efs/share
-sudo /root/.local/bin/aws s3 cp s3://ec2config.toai.stanford.edu/ec2-config.tar.gz /efs/share/ec2config.tar.gz
+sudo /root/.local/bin/aws s3 cp s3://mybucket.stanford.edu/ec2-config.tar.gz /efs/share/ec2config.tar.gz
 sudo tar -xzvf /efs/share/ec2config.tar.gz -C /efs/share
 sudo /bin/cp /efs/share/etc/krb5.conf /etc/krb5.conf
 sudo /bin/cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
